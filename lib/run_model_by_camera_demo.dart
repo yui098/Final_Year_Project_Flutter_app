e71c61dd268d92d3a4b42cd3f70e740858d650b4
yui098 <tamtsnpang@gmail.com>
2024-04-16 13:20:05 +0800

    fix typos

--- lib/run_model_by_camera_demo.dart
+++ lib/run_model_by_camera_demo.dart
@@ -214,7 +214,7 @@ class _RunModelByCameraDemoState extends State<RunModelByCameraDemo> {
         builder: (context) {
           return AlertDialog(
             title: Text("$targetRoute Arrived"),
-            content: Text('Bus Route $targetRoute is arriving, Please get on soon.'),
+            content: Text('Bus Route $targetRoute is arriving, Please get on very soon.'),
             actions: <Widget>[
               TextButton(
                 child: Text("close"),
